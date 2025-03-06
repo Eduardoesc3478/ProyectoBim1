66ac5de1c1c6f99046ab6423ce337dc7c9e21068
Eduardoesc3478c <eduardo.escobar3478@gmail.com>
2025-03-05 21:13:12 -0600

    data y endpoints t

--- src/cart/cart.controller.js
+++ src/cart/cart.controller.js
@@ -1,13 +1,13 @@
 import Cart from './cart.model.js';
 import Product from '../product/product.model.js';
 
-// Método para agregar productos al carrito
+
 export const addToCart = async (req, res) => {
     try {
-        const { userId } = req.params; // Obtener el userId de los parámetros de la solicitud
+        const { userId } = req.params; 
         const { productId, quantity } = req.body;
 
-        // Verificar si el producto existe
+        
         const product = await Product.findById(productId);
         if (!product) {
             return res.status(404).json({
@@ -16,29 +16,29 @@ export const addToCart = async (req, res) => {
             });
         }
 
-        // Verificar si el carrito del usuario ya existe
+        
         let cart = await Cart.findOne({ userId });
 
         if (!cart) {
-            // Crear un nuevo carrito si no existe
+            
             cart = new Cart({
                 userId,
                 products: [{ productId, quantity }]
             });
         } else {
-            // Si el carrito existe, verificar si el producto ya está en el carrito
+
             const product = cart.products.findIndex(p => p.productId.toString() === productId);
 
             if (product > -1) {
-                // Si el producto ya está en el carrito, actualizar la cantidad
+                
                 cart.products[product].quantity += quantity;
             } else {
-                // Si el producto no está en el carrito, agregarlo
+                
                 cart.products.push({ productId, quantity });
             }
         }
 
-        // Guardar el carrito actualizado
+        
         await cart.save();
 
         return res.status(200).json({
@@ -102,10 +102,10 @@ export const purchaseCart = async (req, res) => {
 
 export const deleteProductFromCart = async (req, res) => {
     try {
-        const { userId } = req.params; // Obtener el userId de los parámetros de la solicitud
-        const { productId } = req.body; // Obtener el productId del cuerpo de la solicitud
+        const { userId } = req.params; 
+        const { productId } = req.body; 
 
-        // Verificar si el carrito del usuario existe
+        
         const cart = await Cart.findOne({ userId });
 
         if (!cart) {
@@ -115,7 +115,7 @@ export const deleteProductFromCart = async (req, res) => {
             });
         }
 
-        // Verificar si el producto está en el carrito
+       
         const productIndex = cart.products.findIndex(prod => prod.productId.toString() === productId);
 
         if (productIndex === -1) {
@@ -125,7 +125,7 @@ export const deleteProductFromCart = async (req, res) => {
             });
         }
 
-        // Eliminar el producto del carrito
+        
         cart.products.splice(productIndex, 1);
         await cart.save();
 
